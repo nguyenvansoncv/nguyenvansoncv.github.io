2be58195bf7c370c269ae2f80822d50fa1f488ad
nguyenvansoncv <80382414+nguyenvansoncv@users.noreply.github.com>
2026-03-08 23:38:12 +0700

    Optimize initial load, convert media assets, and update work experience

--- js/main.js
+++ js/main.js
@@ -3,19 +3,21 @@
   "use strict";
 
 //Begin - Window Load
-$(window).load(function(){
-
-
-	//==============___Page Loader___================
-  
-  $('#page-loader').delay(300).fadeOut(400, function(){
-
-  });
-
+// Hide loader quickly; do not wait forever for slow third-party/media resources.
+var loaderHidden = false;
+function hidePageLoader() {
+  if (loaderHidden) {
+    return;
+  }
+  loaderHidden = true;
+  $('#page-loader').delay(150).fadeOut(300, function(){});
   $('#loader-name').addClass('loader-left');
   $('#loader-job').addClass('loader-right');
   $('#loader-animation').addClass('loader-hide');
+}
 
+$(window).on('load', function(){
+  hidePageLoader();
 });
 
 //Begin - Document Ready
@@ -23,6 +25,7 @@ $(document).ready(function(){
 
 //==============___Page Loader___================
   $('#loading-wraper').fadeIn(300);
+  setTimeout(hidePageLoader, 1500);
 
 //==============___Testimonials - owl Carousel___================
  $("#testimonial-carousel").owlCarousel({
